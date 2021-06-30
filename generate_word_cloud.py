813fc8f9206eaaf875d2a65ad48acebb39a6a854
VedantWankhade <vedantwankhade17@gmail.com>
2021-07-01 00:13:33 +0530

    Clean the top output

--- generate_word_cloud.py
+++ generate_word_cloud.py
@@ -1,3 +1,5 @@
+import re
+
 import numpy as np
 import pandas as pd
 from os import path
@@ -6,15 +8,24 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 import matplotlib.pyplot as plt
 
 with open('top.out', 'r') as top_file:
+    # skip first 7 rows
     top_output = top_file.read().split("\n")[7:]
-    # print(top_output)
-    processes = ""
-    for line in top_output:
-        p = line.split(" ")[-1]
-        processes += p + " "
-    print(processes)
+    print(top_output)
+    # skip the last empty string (newline character)
+    for line in top_output[:-1]:
+        # print('1: ', line)
+        # remove extra void spaces
+        line = re.sub(r'\s+', ' ', line).strip()
+        # print('2: ', line)
 
-wc = WordCloud().generate(processes)
-plt.imshow(wc, interpolation='bilinear')
-plt.axis("off")
-plt.show()
+    # print(top_output)
+#     processes = ""
+#     for line in top_output:
+#         p = line.split(" ")[-1]
+#         processes += p + " "
+#     print(processes)
+#
+# wc = WordCloud().generate(processes)
+# plt.imshow(wc, interpolation='bilinear')
+# plt.axis("off")
+# plt.show()

6e7623de1bda7dd762f5651aa1516b12378d760d
VedantWankhade <vedantwankhade17@gmail.com>
2021-07-07 18:13:45 +0530

    User input for desktop environment

--- setwallpaper.sh
+++ setwallpaper.sh
@@ -18,6 +18,16 @@ echo 'Setting up wallpaper'
 
 # gwenview ./wc_wall.png
 
+
+
+echo "Select your desktop environment:"
+echo "1. KDE Plasma"
+echo "2. Gnome"
+
+read de
+
+if [ $de -eq 1 ]; then
+
 # reference to setting kde plasma wallpaper from terminal: https://superuser.com/questions/488232/how-to-set-kde-desktop-wallpaper-from-command-line
 
 ########################### doesn't work correctly yet ##########################
@@ -26,6 +36,10 @@ echo 'Setting up wallpaper'
 
 # hardcoded path to wallpaper
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = 'org.kde.image';d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');d.writeConfig('Image', '$(pwd)/wc_wall.png')}"
-#################################################################################
-
 echo 'Wallpaper set successfully'
+#################################################################################
+fi
+
+if [ $de -eq 2 ]; then
+  echo "Sorry! This does not yet work for gnome."
+fi

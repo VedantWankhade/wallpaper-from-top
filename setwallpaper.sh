dd2d5336ce0d1b99a2f76d1d54c7651cf735dbe9
VedantWankhade <vedantwankhade17@gmail.com>
2021-07-07 18:23:29 +0530

    Message for gnome users

--- setwallpaper.sh
+++ setwallpaper.sh
@@ -21,8 +21,8 @@ echo 'Setting up wallpaper'
 
 
 echo "Select your desktop environment:"
-echo "1. KDE Plasma"
-echo "2. Gnome"
+echo "  1. KDE Plasma"
+echo "  2. Gnome"
 
 read de
 
@@ -34,12 +34,11 @@ if [ $de -eq 1 ]; then
 # only sets the wallpaper if the current wallpaper is not set by the command below
 # in other words this script works only once
 
-# hardcoded path to wallpaper
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript "var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = 'org.kde.image';d.currentConfigGroup = Array('Wallpaper', 'org.kde.image', 'General');d.writeConfig('Image', '$(pwd)/wc_wall.png')}"
 echo 'Wallpaper set successfully'
 #################################################################################
 fi
 
 if [ $de -eq 2 ]; then
-  echo "Sorry! This does not yet work for gnome."
+  echo "Sorry! This does not yet work for gnome. Set wc_wall.png as wallpaper manually."
 fi
